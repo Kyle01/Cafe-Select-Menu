a7e24175d57612c68cfb0bff62ebb974ed1cda30
kyle01 <kylemcv@gmail.com>
2021-09-10 19:01:58 -0400

    Remove eslint error

--- pages/food/index.tsx
+++ pages/food/index.tsx
@@ -2,7 +2,7 @@ export default function Food() {
   return (
       <div>
         <p>Food menu is a work in progress!</p>
-        <p>If you're hungry, ask your server what is available in the kitchen.</p>
+        <p>If you are hungry, ask your server what is available in the kitchen.</p>
       </div>
   )
 }
