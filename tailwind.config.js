214c0005135340d5ea5a3b942f6faf9deaa4e094
kyle01 <kylemcv@gmail.com>
2021-09-10 22:00:05 -0400

    Update tailwind styling colors, update styling  on home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,41 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      darkGreen: {
+        'light': '#A2B9A9',
+        'medium': '#3C5343',
+        'dark': '#092010'
+      },
+      paleTan: {
+        'light': '#FFFFEE',
+        'medium': '#D7D7BB',
+        'dark': '#717155'
+      },
+      paleBlue: {
+        'light': '#D5E8F1',
+        'medium': '#6F828B',
+        'dark': '#091C25'
+      },
+      teaGreen: {
+        'light': '#F7FFD5',
+        'medium': '#C4D2A2',
+        'dark': '#5E6C3C'
+      },
+      midnightBlue: {
+        'light': '#D6CDFF',
+        'medium': '#7067B5',
+        'dark': '#0A014F'
+      },
+    },
   },
   variants: {
     extend: {},
