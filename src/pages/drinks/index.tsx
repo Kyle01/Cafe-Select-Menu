3550406d0fd7561f6486f929d767692d75269c9c
kyle01 <kylemcv@gmail.com>
2021-09-11 11:55:06 -0400

    Introduce filters

--- src/pages/drinks/index.tsx
+++ src/pages/drinks/index.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react'
 import { supabase } from '../../utils/supabaseClient'
+import Pill from '../../components/Pill'
 import { MenuItem } from '../../utils/types'
 import _ from 'lodash'
 
 export default function Drinks() {
   const [drinks, setDrinks] = useState<Array<MenuItem>>([])
+  const [cocktailPillActive, setCocktailPillActive] = useState<boolean>(false)
   
   useEffect(() => {
     fetchItems()
@@ -20,33 +22,49 @@ export default function Drinks() {
   }
 
   return (
-      <div className='m-4'>
-          <h1 className='text-4xl mt-4 mb-4'>Drinks</h1>
-          <h2 className='text-2xl mt-4 mb-4'>Cocktails</h2>
-          {_.filter(drinks, (item) => item.category.toLocaleLowerCase() === 'Cocktails'.toLocaleLowerCase())
-            .map((drink) => (
-            <div key={drink.id} className='mt-2 mb-2'>
-              <p className='font-bold'>{drink.name}</p>
-              <p className='mb-4'>{drink.description}</p>
-            </div>
-          ))}
-          <h2 className='text-2xl mt-4 mb-4'>Wine</h2>
-          {_.filter(drinks, (item) => item.category.toLocaleLowerCase() === 'Wine'.toLocaleLowerCase())
-            .map((drink) => (
-            <div key={drink.id} className='mt-2 mb-2'>
-              <p className='font-bold'>{drink.name}</p>
-              <p className='mb-4'>{drink.description}</p>
-            </div>
-          ))}
-          <h2 className='text-2xl mt-4 mb-4'>Beer</h2>
-          {_.filter(drinks, (item) => item.category.toLocaleLowerCase() === 'Beer'.toLocaleLowerCase())
-            .map((drink) => (
-            <div key={drink.id} className='mt-2 mb-2'>
-              <p className='font-bold'>{drink.name}</p>
-              <p className='mb-4'>{drink.description}</p>
-            </div>
-          ))}
-
-      </div>
+      <body className='bg-paleBlue-light m-0'>
+          <div className='bg-paleBlue-medium p-4'>
+            {cocktailPillActive && 
+              <button 
+                className="py-1 px-2 w-8 cursor:pointer shadow-md rounded-full border text-center border-white text-white text-xs btn-primary focus:outline-none active:shadow-none mr-2"   
+                onClick={() => setCocktailPillActive(false)}
+              >
+                X
+              </button>
+            }
+            <Pill 
+              active={cocktailPillActive}
+              onClick={() => setCocktailPillActive(!cocktailPillActive)}
+              text='Cocktails'
+            />
+          </div>
+          <div className ='p-4 '>
+            <h1 className='text-4xl mt-4 mb-4'>Drinks</h1>
+            <h2 className='text-2xl mt-4 mb-4'>Cocktails</h2>
+            {_.filter(drinks, (item) => item.category.toLocaleLowerCase() === 'Cocktails'.toLocaleLowerCase())
+              .map((drink) => (
+              <div key={drink.id} className='mt-2 mb-2'>
+                <p className='font-bold'>{drink.name}</p>
+                <p className='mb-4'>{drink.description}</p>
+              </div>
+            ))}
+            {!cocktailPillActive && <h2 className='text-2xl mt-4 mb-4'>Wine</h2>}
+            {!cocktailPillActive && _.filter(drinks, (item) => item.category.toLocaleLowerCase() === 'Wine'.toLocaleLowerCase())
+              .map((drink) => (
+              <div key={drink.id} className='mt-2 mb-2'>
+                <p className='font-bold'>{drink.name}</p>
+                <p className='mb-4'>{drink.description}</p>
+              </div>
+            ))}
+            {!cocktailPillActive && <h2 className='text-2xl mt-4 mb-4'>Beer</h2>}
+            {!cocktailPillActive && _.filter(drinks, (item) => item.category.toLocaleLowerCase() === 'Beer'.toLocaleLowerCase())
+              .map((drink) => (
+              <div key={drink.id} className='mt-2 mb-2'>
+                <p className='font-bold'>{drink.name}</p>
+                <p className='mb-4'>{drink.description}</p>
+              </div>
+            ))}
+          </div>
+      </body>
   )
 }
