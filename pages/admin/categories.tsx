ec1e013bed4709d41062f5cd9587708d50b014b9
kyle01 <kylemcv@gmail.com>
2021-09-14 22:48:24 -0400

    Create an about section that can be edited

---
+++ pages/admin/categories.tsx
@@ -0,0 +1,10 @@
+import AdminHeader from '../../components/AdminHeader'
+
+export default function Categories() {
+  return (
+      <div className='m-4'>
+          <AdminHeader />
+          Categories coming soon
+      </div>
+  )
+}
--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -4,9 +4,9 @@ import Wallpaper from '../../public/homeWallpaper.jpg'
 
 export default function Home() {
   return (
-    <>
+    <div className='overflow-hidden'>
       <Image src={Wallpaper} layout='fill' objectFit="cover" objectPosition="center" />
-      <div className='w-screen h-screen justify-center flex flex-col items-center overflow-hidden fixed'>
+      <div className='w-screen h-screen justify-center flex flex-col items-center overflow-hidden'>
         <div className='z-10 fixed bg-darkGreen-dark border-paleBlue-light border-4'>
           <p className='text-center text-paleBlue-medium text-yellow-600 mt-8 mb-8 text-2xl'>Café Select</p>
           <Link href='/drinks'>
@@ -26,6 +26,6 @@ export default function Home() {
           </Link>
         </div>
       </div> 
-    </>
+    </div>
   )
 }
