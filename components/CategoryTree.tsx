416756b5b24a0d9ec578bc50a9686e0b1dd05577
kyle01 <kylemcv@gmail.com>
2021-09-20 23:53:28 -0400

    Small styling changes

--- components/CategoryTree.tsx
+++ components/CategoryTree.tsx
@@ -21,13 +21,14 @@ const About = ({
         <>
             {tree.sort((n, m) => n.path < m.path ? -1 : 1).map((node) => {
                 const nodeLevel = level(node) - 1
+                const header = node.has_header
                 
                 return (
                     <div className='flex justify-between max-w-xl mt-4 mb-4' key={node.name}>
                         <div className={`ml-${(nodeLevel) * 4} text-xl flex items-center`}>
                             <p className='mr-2'>{nodeLevel > 0 ? '|_' : null}</p>
                             <p className='pt-2'>{node.name}</p>
-                            <p className='pt-2 text-sm ml-2'>{node.has_header ? '- Header -' : ' - Hidden - '}</p>
+                            <p className={`pt-2 text-sm ml-2 ${header ? null : 'text-midnightBlue-medium'}`}>{header ? '- Header -' : ' - Hidden - '}</p>
                             <button 
                                 className='bg-paleBlue-medium hover:bg-paleBlue-dark text-white font-bold py-1 px-2 rounded text-xs ml-4'
                                 onClick={() => onDelete(node.id)}
--- utils/routes.ts
+++ utils/routes.ts
@@ -1,4 +1,5 @@
 export enum routes {
+    MENU_HOME = '/home',
     ADMIN_ITEMS = '/admin/items',
     ADMIN_CREATE = '/admin/create',
     ADMIN_CATEGORIES = '/admin/categories',
