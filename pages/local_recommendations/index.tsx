951be19431442d69f2ebf773a41ade7f4566092f
kyle01 <kylemcv@gmail.com>
2021-10-30 07:51:20 -0400

    Create local recommendation page

---
+++ pages/local_recommendations/index.tsx
@@ -0,0 +1,9 @@
+export default function Recommendations() {
+  return (
+    <div className='m-4'>
+      <h1 className='text-4xl mb-2'>Local recommendations</h1>
+      <p className='mb-2'>Local recommendations is a work in progress</p>
+      <p>Ask your server for their favorite local spots</p>
+  </div>
+  )
+}
